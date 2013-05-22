888de890f73bbe0c45cdd46c5db51c98fdcf1328
larryeitel <larry@eitel.com>
2013-05-21 19:01:33 -0600

    working on edit save

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,19 @@ angular.module('of5App', [])
             .when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl'})
             .when('/plcs', {templateUrl: 'views/plcs.html', controller: 'PlcsCtrl'})
             .when('/plc/:id/edit', {templateUrl: 'views/plc-edit.html', controller: 'PlcEditCtrl'})
-            .when('/plc/:id', {templateUrl: 'views/plc-view.html', controller: 'PlcViewCtrl'})
+            .when('/plc/:id', {templateUrl: 'views/plc-view.html', controller: 'PlcViewCtrl'/*,
+                resolve: {
+                    plc: function($route, Plc) {
+                        console.log($route.current.params.id);
+                        Plc.getById($route.current.params.id, function(plc){
+                            try {
+                                return plc;
+                            } catch (_error) {
+//                                error = _error;
+                                return [];
+                            }
+                        });
+                    }}*/})
             .otherwise({redirectTo: '/'});
     })
     .factory('Plc', function (xchglabResourceHttp) {
--- app/scripts/controllers/plcs.js
+++ app/scripts/controllers/plcs.js
@@ -1,64 +1,28 @@
 'use strict';
 
 angular.module('of5App')
-    .controller('PlcsCtrl', ['$scope', '$location', '$routeParams', 'Plc', function ($scope, $location, $routeParams, Plc) {
-
-//        try {
-//            $scope.plcs = plcs['_items'];
-//        } catch (_error) {
-//            error = _error;
-//            $scope.plcs = [];
-//        }
-
-
-        $scope.init = function () {
-            Plc.query({}, {max_results: 5}, function(plcs){
-                $scope.plcs = plcs;
-
-                try {
-                    $scope.plcs = plcs['_items'];
-                } catch (_error) {
-                    error = _error;
-                    $scope.plcs = [];
-                }
-            });
-        };
-
-        $scope.init();
-
-
-
-
-        $scope.edit = function(plc) {
-            return $location.path("/plc/" + plc._id + "/edit");
-        };
-        $scope.view = function(plc) {
-            return $location.path("/plc/" + plc._id);
-        };
-    }])
-
-//    .controller('PlcViewCtrl', ['$scope', '$routeParams', 'Plc', 'User', 'plc', function ($scope, $routeParams, Plc, User, plc) {
-    .controller('PlcViewCtrl', ['$scope', '$routeParams', 'Plc', 'User', function ($scope, $routeParams, Plc, User) {
-//        $scope.plc = plc
-
-
-        $scope.init = function () {
-            Plc.getById($routeParams.id, function(plc){
-                $scope.plc = plc;
-                $scope.lat = plc.pts[0];
-                $scope.lng = plc.pts[1];
-            });
-        };
-
-        $scope.init();
-
-
-    }])
 
     .controller('PlcEditCtrl', ['$scope', '$location', '$routeParams', 'Plc', 'User',
             function ($scope, $location, $routeParams, Plc, User) {
 
         var changeError, changeSuccess, plcCopy;
+
+
+        $scope.update = function () {
+            var actions;
+            console.log('update');
+            actions = {
+                "$set": {
+                    "flds": {
+                        "lbl": $scope.plc.lbl
+                    }
+                }
+            };
+            return $scope.plc.$update(JSON.stringify({
+                "actions": actions
+            }), changeSuccess, changeError);
+        };
+
 
         $scope.init = function () {
             Plc.getById($routeParams.id, function(plc){
@@ -87,6 +51,49 @@ angular.module('of5App')
         return $scope.hasChanges = function() {
             return !angular.equals($scope.plc, plcCopy);
         };
+    }])
 
 
-    }]);
+    .controller('PlcsCtrl', ['$scope', '$location', '$routeParams', 'Plc', function ($scope, $location, $routeParams, Plc) {
+
+        $scope.init = function () {
+            Plc.query({}, {max_results: 5}, function(plcs){
+                $scope.plcs = plcs;
+
+                try {
+                    $scope.plcs = plcs['_items'];
+                } catch (_error) {
+                    error = _error;
+                    $scope.plcs = [];
+                }
+            });
+        };
+
+        $scope.init();
+
+        $scope.edit = function(plc) {
+            return $location.path("/plc/" + plc._id + "/edit");
+        };
+        $scope.view = function(plc) {
+            return $location.path("/plc/" + plc._id);
+        };
+    }])
+
+
+
+    .controller('PlcViewCtrl', ['$scope', '$routeParams', 'Plc', 'User'/*, 'plc'*/, function ($scope, $routeParams, Plc, User/*, plc*/) {
+//        $scope.plc = plc
+
+        $scope.init = function () {
+            Plc.getById($routeParams.id, function(plc){
+                $scope.plc = plc;
+                $scope.lat = plc.pts[0];
+                $scope.lng = plc.pts[1];
+            });
+        };
+
+        $scope.init();
+    }])
+
+
+    ;

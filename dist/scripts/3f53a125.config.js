7e6086d948fb0f7d9356a42b85c54192ddfe6944
larryeitel <larry@eitel.com>
2013-06-02 13:14:04 -0600

    try gplus

--- dist/scripts/3f53a125.config.js
+++ dist/scripts/3f53a125.config.js
@@ -2,5 +2,5 @@
 // of.xchg.com
 angular.module('ofApp')
   .controller('GPlsCtrl', ['$scope', function($scope) {
-    $scope.clientId = '389499396751';
+    $scope.clientId = '389499396751-na26guo6nsi4fvvif45ub9gcpru08q47.apps.googleusercontent.com';
   }]);
